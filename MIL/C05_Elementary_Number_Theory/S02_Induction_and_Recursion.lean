876659ad1ae36b0ac6ac70f49d5684a0c0048f83
Aayush <arajasek94@gmail.com>
2025-11-10 15:06:20 -0500

    Proofs for C05: Induction and Recursion

--- MIL/C05_Elementary_Number_Theory/S02_Induction_and_Recursion.lean
+++ MIL/C05_Elementary_Number_Theory/S02_Induction_and_Recursion.lean
@@ -48,7 +48,14 @@ theorem dvd_fac {i n : ℕ} (ipos : 0 < i) (ile : i ≤ n) : i ∣ fac n := by
 theorem pow_two_le_fac (n : ℕ) : 2 ^ (n - 1) ≤ fac n := by
   rcases n with _ | n
   · simp [fac]
-  sorry
+  simp
+  induction' n with n ih
+  simp [fac]
+  rw [fac, pow_succ, add_assoc, one_add_one_eq_two,  mul_comm]
+  apply Nat.mul_le_mul _ ih
+  linarith
+
+
 section
 
 variable {α : Type*} (s : Finset ℕ) (f : ℕ → ℕ) (n : ℕ)
@@ -99,7 +106,11 @@ theorem sum_id (n : ℕ) : ∑ i ∈ range (n + 1), i = n * (n + 1) / 2 := by
   ring
 
 theorem sum_sqr (n : ℕ) : ∑ i ∈ range (n + 1), i ^ 2 = n * (n + 1) * (2 * n + 1) / 6 := by
-  sorry
+  symm; apply Nat.div_eq_of_eq_mul_right (by norm_num: 0 < 6)
+  induction' n with n ih
+  ring
+  rw [Finset.sum_range_succ, mul_add 6 _ _, ← ih]
+  ring
 end
 
 inductive MyNat where
@@ -134,13 +145,29 @@ theorem add_comm (m n : MyNat) : add m n = add n m := by
   rw [add, succ_add, ih]
 
 theorem add_assoc (m n k : MyNat) : add (add m n) k = add m (add n k) := by
-  sorry
+  induction' n with n ih
+  ·  rw [zero_add, add]
+  rw [succ_add, add, succ_add, ih, add]
+
+
 theorem mul_add (m n k : MyNat) : mul m (add n k) = add (mul m n) (mul m k) := by
-  sorry
+  induction' k with k ih
+  rw [add,mul,add]
+  rw [add, mul, ih, mul, add_assoc]
+
 theorem zero_mul (n : MyNat) : mul zero n = zero := by
-  sorry
+  induction' n with n ih
+  rfl
+  rw [mul, add, ih]
+
 theorem succ_mul (m n : MyNat) : mul (succ m) n = add (mul m n) n := by
-  sorry
+  induction' n with n ih
+  rfl
+  rw [mul, mul, ih, add_assoc, add_assoc, add_comm n, succ_add]
+  rfl
+
 theorem mul_comm (m n : MyNat) : mul m n = mul n m := by
-  sorry
+  induction' n with n ih
+  rw [mul, zero_mul]
+  rw [mul, ih, succ_mul]
 end MyNat
